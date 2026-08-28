69d20ccea05b866d607ba248aef28875737d3f7c
sevahardian <142093439+sevahardian@users.noreply.github.com>
2026-08-28 15:07:34 +0700

    Add files via upload

---
+++ tugas_2.2.py
@@ -0,0 +1,24 @@
+# 1. Konversi angka_string menjadi integer
+angka_string = "123"
+
+# Membuat variabel baru untuk mengubah tipe data variabel angka_string menjadi integer dengan int()
+konversi_angka_string = int(angka_string)
+print (konversi_angka_string, ", data ini telah terkonversi menjadi tipe data", type(konversi_angka_string), "yang awalnya adalah", angka_string, "bertipe data", type(angka_string))
+
+# 2. Konversi angka_float menjadi integer
+angka_float = 45.67
+
+# Membuat variabel baru untuk mengubah tipe data variabel angka_float menjadi integer dengan int()
+konversi_angka_float = int(angka_float)
+print (konversi_angka_float, ", data ini telah terkonversi menjadi tipe data", type(konversi_angka_float), "yang awalnya adalah", angka_float, "bertipe data", type(angka_float))
+
+# Konversi angka_integer ke tipe data lain
+angka_integer = 89
+
+# 3. Konversi angka_integer menjadi float menggunakan variabel baru dengan float()
+konversi_angka_integer_1 = float(angka_integer)
+print (konversi_angka_integer_1, ", data ini telah terkonversi menjadi tipe data", type(konversi_angka_integer_1), "yang awalnya adalah", angka_integer, "bertipe data", type(angka_integer))
+
+# 4. Konversi angka_integer menjadi string menggunakan variabel baru dengan str()
+konversi_angka_integer_2 = str(angka_integer)
+print (konversi_angka_integer_2, ", data ini telah terkonversi menjadi tipe data", type(konversi_angka_integer_2), "yang awalnya adalah", angka_integer, "bertipe data", type(angka_integer))
