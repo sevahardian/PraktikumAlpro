aba4c06cf32f4c2f2832aea0acab2eb5d5243efc
seva17 <sevahardian@gmail.com>
2026-09-04 13:54:59 +0700

    add tugas 1 a dan b

--- Pertemuan 3/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-# 'is' sebagai komperasi object identity (bukan literal)
-print("===== Object Identity (is) =====")
-
-x = 5 # ini adalah assignment membuat object
-y = 5 # ini adalah assignment membuat object
-
-hasil = x is y
-print("x", (x), "is y", (y), "=", hasil)
--- Pertemuan 3/tugas_3.1.py
+++ Pertemuan 3/tugas_3.1.py
@@ -5,6 +5,13 @@ luas = panjang * lebar
 volume = panjang * lebar * tinggi
 keliling = 2 * (panjang + lebar)
 
+# a. Hitunglah luas, volume, dan keliling bangunan tersebut!
 print("Luas permukaan bangunan adalah", luas, "cm^2")
 print("Volume bangunan adalah", volume, "cm^3")
 print("Keliling bangunan adalah", keliling, "cm")
+
+# b. Apakah luas bangunan lebih dari 50 cm^2
+print("Luas bangunan lebih dari 50 cm^2:", luas > 50)
+
+# c. Apakah volume bangunan bernilai 480 cm^3
+print("Volume bangunan sama dengan 480 cm^3:", volume == 480)
