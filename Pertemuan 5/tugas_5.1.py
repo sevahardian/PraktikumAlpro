70bcb107fe375a0a6c11bf9304b0b67c74e1e0f1
seva17 <sevahardian@gmail.com>
2026-09-18 14:29:04 +0700

    revisi tugas_5.1: print(f"...")

--- Pertemuan 5/tugas_5.1.py
+++ Pertemuan 5/tugas_5.1.py
@@ -2,6 +2,6 @@ bilangan = range(1, 51)
 
 for i in bilangan:
     if i % 2 == 0:
-        print("Angka " + str(i) + " merupakan angka genap")
+        print(f"Angka {i} merupakan angka genap")
     else:
-        print("Angka " + str(i) + " merupakan angka ganjil")
+        print(f"Angka {i} merupakan angka ganjil")
